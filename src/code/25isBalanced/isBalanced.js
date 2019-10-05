5758502c3d9cc7b40904ea52390a573907e7eaa3
toast <729440390@qq.com>
2019-10-05 15:35:05 +0800

    feature: add isBalancedTree problem

--- src/code/25isBalanced/isBalanced.js
+++ src/code/25isBalanced/isBalanced.js
@@ -86,13 +86,13 @@ var isBalanced1 = function(root) {
 
 var isBalanced2 = function(root) {
   return process(root)!==-1;
+  
+  function process (node) {
+    if (!node) return 0;
+    let left = process(node.left);
+    if (left===-1) return -1;
+    let right = process(node.right);
+    if (right===-1) return -1;
+    return Math.abs(left - right) < 2 ? Math.max(left, right)+1 : -1;
+  }
 };
-
-function process (node) {
-  if (!node) return 0;
-  let left = process(node.left);
-  if (left===-1) return -1;
-  let right = process(node.right);
-  if (right===-1) return -1;
-  return Math.abs(left - right) < 2 ? Math.max(left, right)+1 : -1;
-}
