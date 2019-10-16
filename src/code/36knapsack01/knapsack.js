ee6b3a2d07a13f76207199b9f8b7aa0e56d601ea
toast <729440390@qq.com>
2019-10-16 16:27:20 +0800

    optimize: capacity from O(N*C) to O(C)

--- src/code/36knapsack01/knapsack.js
+++ src/code/36knapsack01/knapsack.js
@@ -86,4 +86,30 @@ function knapsack3(w, v, c) {
 console.log(knapsack3([3, 1, 2], [6, 4, 3], 5))
   
 
+// dp 空间优化 只使用两个数组空间
+function knapsack4(w, v, c) {
+  const n = w.length
 
+  const memo = []
+  for(let i = 0; i < 2; i++) {
+    memo.push(new Array(c+1).fill(-1))
+  }
+
+  for(let i = 0; i <= c; i++) {
+    memo[0][i] = ( i >= w[0] ? v[0] : 0)
+  }
+  
+  for(let i = 1; i < n; i++) {
+    for(let j = 0; j <=c; j++) {
+      memo[i % 2][j] = memo[(i-1) % 2][j]
+      if(j >= w[i]) {
+        memo[i % 2][j] = Math.max(memo[i % 2][j], v[i] + memo[(i-1) % 2][j-w[i]])
+      }
+    }
+  }
+
+  return memo[(n-1) % 2][c]
+}
+
+console.log(knapsack4([3, 1, 2], [6, 4, 3], 5))
+  
