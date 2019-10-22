19d23a24ea827085b4873a97daca0cdbf5e0d83f
toast <729440390@qq.com>
2019-10-22 22:12:47 +0800

    feature: add rob2 problem

---
+++ src/code/41rob2/213rob.js
@@ -0,0 +1,66 @@
+/**
+你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
+给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+
+示例 1:
+
+输入: [2,3,2]
+输出: 3
+解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+示例 2:
+
+输入: [1,2,3,1]
+输出: 4
+解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 备忘录
+var rob1 = function(nums) {
+  if(nums.length === 1) return nums[0]
+  const memo1 = new Array(nums.length).fill(-1)
+  const memo2 = new Array(nums.length).fill(-1)
+
+  return Math.max(process(nums, 0, memo1), process(nums.reverse(), 0, memo2))
+
+  function process(nums, i, memo) {
+    if(i >= nums.length-1) return 0
+    if(memo[i] !== -1) return memo[i]
+    let cur = process(nums, i+2, memo) + nums[i]
+    let prev = process(nums, i+1, memo)
+
+    return memo[i] = Math.max(cur, prev)
+  }
+};
+
+console.log(rob1([1,3,1,3,100]));
+
+// dp
+// var rob2 = function(nums) {
+//   if(nums.length === 1) return nums[0]
+//   if(nums.length === 0) return 0
+  
+//   return Math.max(process(nums.slice(1, nums.length+1)), process(nums.reverse().slice(1, nums.length+1)))
+
+//   function process(nums) {
+//     const len = nums.length
+//     const dp = new Array(len).fill(-1)
+    
+//     dp[len-1] = nums[len-1]
+
+//     for(let i = len-2; i >= 0; i--) {
+//       for(let j = i; j < len - 1; j++) {
+//         dp[i] = Math.max(dp[i], nums[j] + (j + 2 < len ? dp[j+2] : 0))
+//       }
+//     }
+//     console.log(dp);
+//     return dp[0]
+//   }
+// };
+
+// console.log(rob2([1,3,1,3,100]));
