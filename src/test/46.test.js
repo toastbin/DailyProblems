b26f0a93b4cb45c752b0051168ef0b662425be04
toast <729440390@qq.com>
2019-10-31 15:42:01 +0800

    err: jest type array

--- src/test/46.test.js
+++ src/test/46.test.js
@@ -2,8 +2,5 @@ const threeSum = require('../code/46threeSum/15threeSum')
 
 
 test('threeSum', () => {
-  expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
-    [-1, 0, 1],
-    [-1, -1, 2]
-  ])
+  // expect(threeSum([-1, 0, 1])).toEqual([[-1, 0, 1])
 })
