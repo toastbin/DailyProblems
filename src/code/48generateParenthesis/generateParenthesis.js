cb9b04a909724c56d6e7e1c6a7805de6e7d54a37
toast <729440390@qq.com>
2019-11-02 15:27:51 +0800

    feature: add generateParenthesis problem

---
+++ src/code/48generateParenthesis/generateParenthesis.js
@@ -0,0 +1,38 @@
+/**
+给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+例如，给出 n = 3，生成结果为：
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+*/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  const res = []
+  process(res, '', 0, 0, n)
+
+  return res
+
+  function process(res, str, l, r, n) {
+    if(l > n || r > n || r > l) return
+    if(l === n && r === n ) {
+      res.push(str)
+    }
+    process(res, str + '(', l + 1, r, n)
+    process(res, str + ')', l, r + 1, n)
+    return
+  }
+};
+
+
+console.log(generateParenthesis(3));
