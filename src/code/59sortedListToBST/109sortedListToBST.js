2fa782270bb92456e7f15034a54b36e0c0ec32df
toastbin <a6046685@163.com>
2020-09-22 10:17:43 +0800

    feat: add sortedListToBST

---
+++ src/code/59sortedListToBST/109sortedListToBST.js
@@ -0,0 +1,54 @@
+/*
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    if (!head) return null
+    if (!head.next) return new TreeNode(head.val)
+    let pre = head
+    let slow = head.next
+    let fast = slow.next
+    while (fast && fast.next) {
+        pre = pre.next
+        slow = slow.next
+        fast = fast.next.next
+    }
+    pre.next = null
+    const root = new TreeNode(slow.val)
+    root.left = sortedListToBST(head)
+    root.right = sortedListToBST(slow.next)
+    return root
+}
